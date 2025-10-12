c1cc5c484f167db49ab32c4076c34c115c694e5e
Andrii Kurdiumov <kant2002@gmail.com>
2025-10-12 17:01:47 +0500

    Make application run on Java 25 - Update Gradlew to 9.1.0 - Update foojay-resolver-convention to 1.0.0 - Add application plugin for gradlew run support

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.2.20"
+    application
 }
 
 group = "kz.qwertukg"
@@ -20,4 +21,8 @@ kotlin {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+application {
+    mainClass.set("MainKt")
 }
